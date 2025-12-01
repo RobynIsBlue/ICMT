116a14dacb3710fa5cbc92904a41711b07fd814e
RobynIsBlue <tyglivermont@gmail.com>
2025-12-01 12:54:52 -0600

    set up, need to css

---
+++ ICMT3/src/main.js
@@ -0,0 +1,101 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <body>
+    <div id="root">
+      <section id="hero">
+        <div class="container">
+          <h1>
+            <i>WHAT IS<br>MYSTERY THEATRE?</i>
+          </h1>
+          <img src="src/assets/DIVIDER1.png"/>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At consectetur nihil necessitatibus cum excepturi ut, dolor, autem fugit itaque quod praesentium quia tempore esse iure error quidem, in iste pariatur? Ex voluptatibus asperiores sunt molestias, fugiat distinctio maxime laudantium iusto veniam facere inventore fugit officiis sint ducimus voluptates dolorem id.</p>
+        </div>
+      </section>
+    <section id="whatis" class="question">
+      <div class="row">
+        <div>
+          <h1>
+            <i>WHAT IS IMPROBABLE CAUSE?</i>
+          </h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias corrupti modi, provident dolore qui consequuntur suscipit laborum accusamus a atque iste? Quibusdam eum velit reiciendis nesciunt sed aut saepe ex error. Velit voluptatibus et voluptas neque asperiores ex libero eaque excepturi aliquid quis iste, numquam aspernatur praesentium maxime corporis at!</p>
+          <img src="/src/assets/BETTY.png" style="max-width: 200px;">
+        </div>
+      </div>
+    </section>
+    <section id="comesee" class="question">
+      <div>
+        <h1>
+          <i>COME SEE US</i>
+        </h1>
+        <p>I will be a collapsed table where you can uncollapse me and see posters for the events. I will initially show the location, time, and show name.</p>
+        <img src="/src/assets/BETTY.png" style="max-width: 200px;">
+      </div>
+    </section>
+    <section id="testimony" >
+      <div>
+        <h1>
+          <i>DON'T JUST TAKE IT FROM US</i>
+        </h1>
+        <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum laboriosam perspiciatis quidem quam rem amet dolor, ab error voluptates, ea quaerat veniam nobis! Inventore, nam!"</p>
+        <p>- person</p>
+        <img src="/src/assets/BETTY.png" style="max-width: 200px;">
+        <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates a harum fugit facilis nemo repellat accusamus animi. Voluptates facilis, quisquam deleniti nobis harum culpa distinctio!"</p>
+        <p>- person</p>
+        <img src="/src/assets/BETTY.png" style="max-width: 200px;">
+      </div>
+    </section
+    <div>
+      <h1>
+        <i>DO YOU HAVE A VENUE?</i>
+      </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque porro rem ipsa accusantium illum impedit itaque officia voluptatum! Recusandae repudiandae fugit ad quas ullam nobis vel hic corporis! Quas dicta ea optio excepturi nesciunt sit totam expedita facere deserunt eum.</p>
+      <img src="/src/assets/BETTY.png" style="max-width: 200px;">
+    </div>
+    <div>
+      <div>
+        <h1>
+          <i>PHONE</i>
+        </h1>
+        <img src="/src/assets/BETTY.png" style="max-width: 200px;">
+      </div>
+      <div>
+        <h1>
+          <i>EMAIL</i>
+        </h1>
+      <img src="/src/assets/BETTY.png" style="max-width: 200px;">
+      </div>
+      <div>
+        <h1>
+          <i>CAST LIST</i>
+        </h1>
+      <img src="/src/assets/BETTY.png" style="max-width: 200px;">
+      </div>
+    </div>
+    <div>
+      <h1>
+        <i>WHODUNNIT?</i>
+      </h1>
+      <div>
+        <h2>BETTY</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eveniet doloribus magni temporibus cum iure ad debitis voluptatem atque maxime?</p>
+        <p>Button for contacting!</p>
+        <img src="/src/assets/BETTY.png" style="max-width: 200px;">
+      </div>
+      <div>
+        <h2>MORETTI</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur suscipit ex dolore illo placeat mollitia unde eius veniam repellendus blanditiis.</p>
+        <img src="/src/assets/BETTY.png" style="max-width: 200px;">
+      </div>
+      <div>
+        <h2>LAURA</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non repellendus mollitia facere exercitationem neque dolor maiores aliquam atque. Expedita, obcaecati.</p>
+        <p>Button for contacting!</p>
+        <img src="/src/assets/BETTY.png" style="max-width: 200px;">
+      </div>
+    </div>
+  </div>
+
+  <script>console.log(WHATIS)</script>
+</body>
+`;
